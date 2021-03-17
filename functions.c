22bee1aebb08822d9a03b483bcb24512cf2ba3fa
luismelendez94 <2709@holbertonschool.com>
2021-03-17 03:07:03 +0000

    Deleting abs function

--- functions.c
+++ functions.c
@@ -4,6 +4,7 @@
  * _putchar - writes the character c to stdout
  * @c: The character to print
  * Return: On error, -1 is returned, and errno is set appropriately.
+ * Description: Function to print characters
  */
 int _putchar(char c)
 {
