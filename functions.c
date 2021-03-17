475d48abaa6b43074ac86bc508d9126006bf83f8
luismelendez94 <2709@holbertonschool.com>
2021-03-17 03:05:53 +0000

    Deleting abs function

--- functions.c
+++ functions.c
@@ -9,30 +9,3 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-
-/**
- * _abs - prints absolute value
- * @r: int sents to function
- *
- * Return: 0
- */
-int _abs(int r)
-{
-
-	int mod_r = (r * -1);
-
-	if (r > 0)
-	{
-		return (r);
-	}
-
-	else if (r < 0)
-	{
-		return (mod_r);
-	}
-
-	else
-
-		return (0);
-
-}
